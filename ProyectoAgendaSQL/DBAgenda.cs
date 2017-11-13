3c191631e30d7dc07f7fb8ea86e2ebfebe61c5d1
vladimir <ivladimirjuarez@outlook.com>
2017-11-13 10:02:59 -0600

    inicio

--- ProyectoAgendaSQL/DBAgenda.cs
+++ ProyectoAgendaSQL/DBAgenda.cs
@@ -12,7 +12,7 @@ namespace ProyectoAgendaSQL
         static SqlConnection conexion;
         public static void DBConectar()
         {
-            conexion = new SqlConnection("Data Source=LAPTOP-7JPH5U2H\\SQLExpress;Initial Catalog=DB_Agenda;Integrated Security=True");
+            conexion = new SqlConnection("Data Source=LAPTOP-Q2ET4LU7\\SQLEXPRESS;Initial Catalog=AgendaEmpresarial;Integrated Security=True;Pooling=False");
             conexion.Open();
         }
 
