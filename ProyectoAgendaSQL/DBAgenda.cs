085b4f91055f8b63e5f7230f8c3fb84e90dc1d5b
vladimir <ivladimirjuarez@outlook.com>
2017-11-13 23:09:53 -0600

    arreglo de Lista Busqueda
    se agregó el join a la clase que hace binding con la base de datos

--- ProyectoAgendaSQL/DBAgenda.cs
+++ ProyectoAgendaSQL/DBAgenda.cs
@@ -93,13 +93,18 @@ namespace ProyectoAgendaSQL
             return listaSucursales;
         }
 
+        /////////////////////////////////////////////////////////////////////////////
+        //problemas con el DataReader
+
         public static Departamento BusquedaIdDepartamento(int id)
         {
             Departamento buscado;
             string consulta = string.Format("SELECT * FROM Departameto WHERE Id='{0}'", id);
             SqlCommand comando = new SqlCommand(consulta, conexion);
             SqlDataReader reader = comando.ExecuteReader();
-            return buscado = new Departamento(id, reader.GetString(1), reader.GetString(2));
+            buscado = new Departamento(id, reader.GetString(1), reader.GetString(2));
+            reader.Close();
+            return buscado;
         }
 
         public static Sucursal BusquedaIdSucursal(int id)
@@ -108,15 +113,19 @@ namespace ProyectoAgendaSQL
             string consulta = string.Format("SELECT * FROM Sucursal WHERE Id='{0}'", id);
             SqlCommand comando = new SqlCommand(consulta, conexion);
             SqlDataReader reader = comando.ExecuteReader();
-            return buscado = new Sucursal(id, reader.GetString(1), reader.GetString(2));
-
+            buscado = new Sucursal(id, reader.GetString(1), reader.GetString(2));
+            reader.Close();
+            return buscado;
         }
 
-
+        //primer metodo que llama al buscar
+        //se abre el primer DataReader y al entrar en la función BusquedaIdDepartamento 
+        //entra en un segundo DataREader arrojando una exepción 
         public static List<Empleado> BuscarEmpleadosModificarEliminar(string nombreBuscar)
         {
             List<Empleado> listaEmpleados = new List<Empleado>();
-            string consulta = string.Format("SELECT Id, Nombre, Telefono, Fax, Email, Departamento, Sucursal FROM Empleado WHERE Nombre like '%{0}%'", nombreBuscar);
+            //string consulta = string.Format("SELECT Id, Nombre, Telefono, Fax, Email, Departamento, Sucursal FROM Empleado WHERE Nombre like '%{0}%'", nombreBuscar);
+            string consulta = string.Format("SELECT E.Id, E.Nombre, E.Telefono, E.Fax, E.Email, D.Nombre, S.Nombre, E.Departamento, E.Sucursal  FROM [Empleado] E JOIN Departamento D ON E.Departamento = D.Id JOIN Sucursal S ON E.Sucursal = S.Id WHERE E.Nombre like '%{0}%'", nombreBuscar);
             SqlCommand comando = new SqlCommand(consulta, conexion);
             SqlDataReader reader = comando.ExecuteReader();
 
@@ -128,8 +137,10 @@ namespace ProyectoAgendaSQL
                 aux.Telefono = reader.GetString(2);
                 aux.Fax = reader.GetString(3);
                 aux.Email = reader.GetString(4);
-                aux.Departamento = BusquedaIdDepartamento(reader.GetInt32(5));
-                aux.Sucursal= BusquedaIdSucursal(reader.GetInt32(6));
+                //aux.Departamento = BusquedaIdDepartamento(reader.GetInt32(5));
+                aux.Departamento = new Departamento(reader.GetInt32(7), reader.GetString(5));
+                //aux.Sucursal= BusquedaIdSucursal(reader.GetInt32(6));
+                aux.Sucursal = new Sucursal(reader.GetInt32(8), reader.GetString(6));
                 listaEmpleados.Add(aux);
             }
 
